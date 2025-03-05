e816e1c9f5a250c20cfcc41006d33bf513c51431
Art <admin@artgurianov.com>
2025-03-05 13:19:15 +0900

    types fixed

--- src/components/BackgroundModel/BackgroundModelObject.tsx
+++ src/components/BackgroundModel/BackgroundModelObject.tsx
@@ -3,21 +3,26 @@
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { Group, Mesh } from "three";
 
 useGLTF.preload("/3DModel.glb");
 
 export default function BackgroundModelObject() {
-  const groupRef = useRef<any>(null);
+  const groupRef = useRef<Group | null>(null);
   const { nodes, materials } = useGLTF("/3DModel.glb");
 
-  useFrame(() => (groupRef.current.rotation.y += 0.01));
+  useFrame(() => {
+    if (groupRef.current) {
+      groupRef.current.rotation.y += 0.01;
+    }
+  });
 
   return (
     <group ref={groupRef} dispose={null}>
       <mesh
         castShadow
         receiveShadow
-        geometry={(nodes.Asset_1 as any).geometry}
+        geometry={(nodes.Asset_1 as Mesh).geometry}
         material={materials["Material.001"]}
         rotation={[Math.PI / 2, 0, 0]}
       />
