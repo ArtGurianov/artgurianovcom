a166ef632f71d367948c2717c602e96250729f3f
Art <admin@artgurianov.com>
2025-03-26 11:58:49 +0900

    translation and styles

--- src/components/CreationProjects/CreationProjectsIntro.tsx
+++ src/components/CreationProjects/CreationProjectsIntro.tsx
@@ -1,8 +1,11 @@
+import { useTranslations } from "next-intl";
+
 export const CreationProjectsIntro = () => {
+  const t = useTranslations("CREATION");
   return (
     <div className="relative w-full h-full">
-      <h1 className="absolute top-1/2 -translate-y-1/2 w-full text-6xl text-start px-12 font-mono text-muted/80 py-8 bg-linear-to-r from-primary/40 via-primary/10 to-primary/0">
-        {"PRODUCTS SHOWCASE"}
+      <h1 className="absolute top-1/2 -translate-y-1/2 w-full text-4xl md:text-6xl px-6 md:px-12 font-mono text-muted/80 py-8 bg-linear-to-r from-primary/40 via-primary/10 to-primary/0">
+        {t("heading")}
       </h1>
     </div>
   );
--- src/components/CreationProjects/CreationProjectsItem.tsx
+++ src/components/CreationProjects/CreationProjectsItem.tsx
@@ -8,7 +8,7 @@ export const CreationProjectsItem = ({
   bgUrl,
 }: CreationProjectData) => {
   return (
-    <div className={"relative bg-black/30 w-full h-full"}>
+    <div className={"relative w-full h-full"}>
       <span className="absolute text-4xl w-full text-center top-1/2 -translate-y-1/2 text-primary font-serif">
         {title}
       </span>
