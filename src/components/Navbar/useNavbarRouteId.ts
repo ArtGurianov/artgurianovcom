050b853406d761cb7d49bf42aa36056f980fcf4c
Art <admin@artgurianov.com>
2025-04-01 16:20:22 +0900

    nav routes fix

--- src/components/Navbar/useNavbarRouteId.ts
+++ src/components/Navbar/useNavbarRouteId.ts
@@ -1,12 +1,19 @@
 import { usePathname } from "next/navigation";
-import { ROUTER_CONFIG } from "../../config/routing/routerConfig";
+import {
+  APP_ROUTE_IDS,
+  ROUTER_CONFIG,
+} from "../../config/routing/routerConfig";
 import { NAVBAR_ROUTE_IDS, NavbarRouteId } from "./config";
 
 export const useNavbarRouteId = () => {
   const pathname = usePathname();
+  if (pathname === "/") return APP_ROUTE_IDS.IDENTITY;
 
   for (const routeId of NAVBAR_ROUTE_IDS) {
-    if (pathname.startsWith(ROUTER_CONFIG[routeId as NavbarRouteId].urlPath)) {
+    if (
+      routeId !== APP_ROUTE_IDS.IDENTITY &&
+      pathname.startsWith(ROUTER_CONFIG[routeId as NavbarRouteId].urlPath)
+    ) {
       return routeId;
     }
   }
