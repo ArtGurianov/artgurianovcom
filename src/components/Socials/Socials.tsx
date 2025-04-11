640b5f32e5810ea55393de247250a0ade1525e91
Art <admin@artgurianov.com>
2025-04-11 16:16:45 +0900

    translations

--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -5,30 +5,90 @@ import {
   CarouselContent,
   CarouselItem,
 } from "@/components/ui/carousel";
-import { SOCIAL_MEDIA_IDS, SocialMediaId, SocialsCard } from "./SocialsCard";
+import { SocialsCard } from "./SocialsCard";
 import { useBreakpoint } from "@/lib/hooks/useBreakpoint";
 import { useState } from "react";
 import { useClickOutside } from "@/lib/hooks";
+import { useTranslations } from "next-intl";
 import Autoscroll from "embla-carousel-auto-scroll";
+import { ValueOf } from "@/lib/types";
 
-const SOCIALS_ORDER: SocialMediaId[] = [
-  SOCIAL_MEDIA_IDS.YOUTUBE,
-  SOCIAL_MEDIA_IDS.MEDIUM,
-  SOCIAL_MEDIA_IDS.TWITTER,
-  SOCIAL_MEDIA_IDS.TELEGRAM,
-  SOCIAL_MEDIA_IDS.HABR,
-  SOCIAL_MEDIA_IDS.INSTAGRAM,
+export const SOCIALS_IDS = {
+  YOUTUBE: "YOUTUBE",
+  TELEGRAM: "TELEGRAM",
+  INSTAGRAM: "INSTAGRAM",
+  HABR: "HABR",
+  MEDIUM: "MEDIUM",
+  TWITTER: "TWITTER",
+} as const;
+export type SocialsId = ValueOf<typeof SOCIALS_IDS>;
+
+export interface SocialsDataItem {
+  imagePath: string;
+  url: string;
+  language: "ru" | "en";
+  isAvailable: boolean;
+}
+
+const SOCIALS_DATA: Record<SocialsId, SocialsDataItem> = {
+  [SOCIALS_IDS.YOUTUBE]: {
+    imagePath: "/socials/youtube.png",
+    url: "youtube.com",
+    language: "en",
+    isAvailable: false,
+  },
+  [SOCIALS_IDS.TWITTER]: {
+    imagePath: "/socials/twitter.png",
+    url: "x.com",
+    language: "en",
+    isAvailable: false,
+  },
+  [SOCIALS_IDS.HABR]: {
+    imagePath: "/socials/habr.png",
+    url: "habr.com",
+    language: "ru",
+    isAvailable: false,
+  },
+  [SOCIALS_IDS.MEDIUM]: {
+    imagePath: "/socials/medium.png",
+    url: "medium.com",
+    language: "en",
+    isAvailable: false,
+  },
+  [SOCIALS_IDS.TELEGRAM]: {
+    imagePath: "/socials/telegram.png",
+    url: "telegram.com",
+    language: "ru",
+    isAvailable: false,
+  },
+  [SOCIALS_IDS.INSTAGRAM]: {
+    imagePath: "/socials/instagram.png",
+    url: "instagram.com",
+    language: "ru",
+    isAvailable: false,
+  },
+};
+
+const SOCIALS_ORDER: SocialsId[] = [
+  SOCIALS_IDS.YOUTUBE,
+  SOCIALS_IDS.MEDIUM,
+  SOCIALS_IDS.TWITTER,
+  SOCIALS_IDS.TELEGRAM,
+  SOCIALS_IDS.HABR,
+  SOCIALS_IDS.INSTAGRAM,
 ];
 
 export const Socials = () => {
   const isWindowOverSM = useBreakpoint("sm");
-  const [activeId, setActiveId] = useState<SocialMediaId | null>(null);
-  const handleChangeActiveId = (value: SocialMediaId | null) => {
+  const [activeId, setActiveId] = useState<SocialsId | null>(null);
+  const handleChangeActiveId = (value: SocialsId | null) => {
     setActiveId(value);
   };
   const containerRef = useClickOutside<HTMLDivElement>(() => {
     setActiveId(null);
   });
+
+  const t = useTranslations("SOCIALS");
 
   return (
     <div className="h-32 w-full md:h-full md:px-4">
@@ -56,9 +116,11 @@ export const Socials = () => {
             >
               <SocialsCard
                 key={id}
-                socialMediaId={id}
+                id={id}
                 activeId={activeId}
                 onChangeActiveId={handleChangeActiveId}
+                description={t(`descriptions.${id}`)}
+                {...SOCIALS_DATA[id]}
               />
             </CarouselItem>
           ))}
--- src/components/Socials/SocialsCard.tsx
+++ src/components/Socials/SocialsCard.tsx
@@ -1,108 +1,47 @@
-import { ValueOf } from "@/lib/types";
 import { cn } from "@/lib/utils";
+import { SocialsDataItem, SocialsId } from "./Socials";
 import Image from "next/image";
 
-export const SOCIAL_MEDIA_IDS = {
-  YOUTUBE: "YOUTUBE",
-  TELEGRAM: "TELEGRAM",
-  INSTAGRAM: "INSTAGRAM",
-  HABR: "HABR",
-  MEDIUM: "MEDIUM",
-  TWITTER: "TWITTER",
-} as const;
-export type SocialMediaId = ValueOf<typeof SOCIAL_MEDIA_IDS>;
-
-interface SocialMediaDataItem {
-  imagePath: string;
-  url: string;
+interface SocialsCardProps extends SocialsDataItem {
+  id: SocialsId;
+  activeId: SocialsId | null;
+  onChangeActiveId: (_: SocialsId | null) => void;
   description: string;
-  language: "ru" | "en";
-  isAvailable: boolean;
-}
-
-const SOCIAL_MEDIA_DATA: Record<SocialMediaId, SocialMediaDataItem> = {
-  [SOCIAL_MEDIA_IDS.YOUTUBE]: {
-    imagePath: "/socials/youtube.png",
-    url: "youtube.com",
-    description: "Youtube channel with full coding tutorials and short hints.",
-    language: "en",
-    isAvailable: false,
-  },
-  [SOCIAL_MEDIA_IDS.TWITTER]: {
-    imagePath: "/socials/twitter.png",
-    url: "x.com",
-    description:
-      "Twitter channel with thoughts and posts about mentality of an engineer.",
-    language: "en",
-    isAvailable: false,
-  },
-  [SOCIAL_MEDIA_IDS.HABR]: {
-    imagePath: "/socials/habr.png",
-    url: "habr.com",
-    description: "Habr tech blog with text format of coding tutorials.",
-    language: "ru",
-    isAvailable: false,
-  },
-  [SOCIAL_MEDIA_IDS.MEDIUM]: {
-    imagePath: "/socials/medium.png",
-    url: "medium.com",
-    description: "Medium tech blog with text format of coding tutorials.",
-    language: "en",
-    isAvailable: false,
-  },
-  [SOCIAL_MEDIA_IDS.TELEGRAM]: {
-    imagePath: "/socials/telegram.png",
-    url: "telegram.com",
-    description:
-      "Telegram channel with thoughts and posts about mentality of an engineer.",
-    language: "ru",
-    isAvailable: false,
-  },
-  [SOCIAL_MEDIA_IDS.INSTAGRAM]: {
-    imagePath: "/socials/instagram.png",
-    url: "instagram.com",
-    description:
-      "Personal instagram with lifestyle of an independend nomad SaaS enterpreneur.",
-    language: "ru",
-    isAvailable: false,
-  },
-};
-
-interface SocialsCardProps {
-  socialMediaId: SocialMediaId;
-  activeId: SocialMediaId | null;
-  onChangeActiveId: (_: SocialMediaId | null) => void;
 }
 
 export const SocialsCard = ({
-  socialMediaId,
+  imagePath,
+  url,
+  language,
+  id,
   activeId,
   onChangeActiveId,
+  description,
 }: SocialsCardProps) => {
   return (
     <div
       className="h-full w-full relative border-2 rounded-2xl border-dashed bg-gradient-to-br from-primary/30 via-primary-10 to-primary/60 overflow-clip"
       onMouseOver={() => {
-        onChangeActiveId(socialMediaId);
+        onChangeActiveId(id);
       }}
       onMouseOut={() => {
         onChangeActiveId(null);
       }}
       onClick={() => {
-        onChangeActiveId(socialMediaId);
+        onChangeActiveId(id);
       }}
     >
       <Image
-        src={SOCIAL_MEDIA_DATA[socialMediaId].imagePath}
-        alt={`Image for ${socialMediaId}`}
+        src={imagePath}
+        alt={`Image for ${id}`}
         width="0"
         height="0"
         sizes="100vh"
         className={cn(
           "h-full w-full object-contain shrink-0 transition-all ease-in-out duration-300",
           {
-            "-translate-y-full": socialMediaId === activeId,
-            "translate-y-0": socialMediaId !== activeId,
+            "-translate-y-full": id === activeId,
+            "translate-y-0": id !== activeId,
           }
         )}
         priority
@@ -111,20 +50,14 @@ export const SocialsCard = ({
         className={cn(
           "text-sm md:text-normal w-full h-full shrink-0 flex flex-col justify-center items-center transition-all ease-in-out duration-300",
           {
-            "-translate-y-full": socialMediaId === activeId,
-            "translate-y-0": socialMediaId !== activeId,
+            "-translate-y-full": id === activeId,
+            "translate-y-0": id !== activeId,
           }
         )}
       >
-        <span className="text-center">
-          {SOCIAL_MEDIA_DATA[socialMediaId].description}
-        </span>
-        <span className="text-center">
-          {SOCIAL_MEDIA_DATA[socialMediaId].language}
-        </span>
-        <span className="text-center">
-          {SOCIAL_MEDIA_DATA[socialMediaId].url}
-        </span>
+        <span className="text-center">{description}</span>
+        <span className="text-center">{language}</span>
+        <span className="text-center">{url}</span>
       </div>
     </div>
   );
