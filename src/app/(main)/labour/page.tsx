1a530cd44f6237a2a901b185f9b6652356dfedfd
Art <admin@artgurianov.com>
2025-05-27 17:30:44 +0300

    styling and translation - labour page

--- src/app/(main)/labour/page.tsx
+++ src/app/(main)/labour/page.tsx
@@ -1,11 +1,16 @@
 import { PageContent } from "@/components/common/PageContent/PageContent";
 import { Button } from "@/components/ui/button";
+import { getAppLocale } from "@/lib/utils";
+import { getTranslations } from "next-intl/server";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function LabourPage() {
+export default async function LabourPage() {
+  const locale = getAppLocale();
+  const t = await getTranslations({ locale, namespace: "LABOUR" });
+
   return (
-    <PageContent className="flex flex-col md:flex-row grow px-8 pb-8 md:px-32 gap-4 md:gap-12">
+    <PageContent className="flex flex-col md:flex-row grow px-8 pb-8 md:px-32 gap-8 md:gap-12">
       <Button
         variant="ghost"
         size="reset"
@@ -22,12 +27,15 @@ export default function LabourPage() {
             fill
             priority
           />
-          <div className="absolute z-10 w-full h-full bg-secondary/30 hover:bg-secondary/50" />
+          <div
+            className="absolute z-10 w-full h-full bg-secondary/30 hover:bg-secondary/50"
+            style={{ boxShadow: "0 0 100px rgba(0,0,0,0.9) inset" }}
+          />
           <span className="absolute top-1/2 -translate-y-1/2 left-0 z-20 w-full text-border/80 font-mono text-center text-wrap text-6xl">
-            {"RECRUIT"}
+            {t("recruit-label")}
           </span>
-          <span className="absolute bottom-4 md:bottom-8 left-0 z-20 text-border/80 bg-background/40 w-full font-semibold font-mono text-center text-wrap text-lg">
-            {"DESCRIPTION"}
+          <span className="absolute bottom-4 md:bottom-8 left-0 z-20 text-border/80 bg-gradient-to-r from-background/10 via-background/50 to-background/10 w-full font-semibold font-mono text-center text-wrap text-xl py-2 px-4">
+            {t("recruit-description")}
           </span>
         </Link>
       </Button>
@@ -47,12 +55,15 @@ export default function LabourPage() {
             fill
             priority
           />
-          <div className="absolute z-10 w-full h-full bg-secondary/30 hover:bg-secondary/50" />
+          <div
+            className="absolute z-10 w-full h-full bg-secondary/30 hover:bg-secondary/50"
+            style={{ boxShadow: "0 0 100px rgba(0,0,0,0.9) inset" }}
+          />
           <span className="absolute top-1/2 -translate-y-1/2 left-0 z-20 w-full text-border/80 font-mono text-center text-wrap text-6xl">
-            {"STUDIO"}
+            {t("studio-label")}
           </span>
-          <span className="absolute bottom-4 md:bottom-8 left-0 z-20 text-border/80 bg-background/40 w-full font-semibold font-mono text-center text-wrap text-lg">
-            {"DESCRIPTION"}
+          <span className="absolute bottom-4 md:bottom-8 left-0 z-20 text-border/80 bg-gradient-to-r from-background/10 via-background/50 to-background/10 w-full font-semibold font-mono text-center text-wrap text-xl py-2 px-4">
+            {t("studio-description")}
           </span>
         </Link>
       </Button>
